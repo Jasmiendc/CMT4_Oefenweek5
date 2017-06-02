7235dc05d13b1ba6f017896071611a9b6d05707b
Jasmiendc <jasmien.declerck@student.ap.be>
2017-06-02 03:21:37 +0200

    Add files via upload

--- root_router.js
+++ root_router.js
@@ -5,6 +5,7 @@ root.get('/', function(req, res) {
   res.render("index", {
     nieuws: req.app.get('nieuwsFile').nieuws,
     categorieen: req.app.get('categorieenFile').categorieen,
+    auteur: req.app.get('auteurFile').auteur
   });
 });
 
